3330da5684f064ffe75a600210d571fcd13256b7
QedirMahmudov <1qedirmahmudov@gmail.com>
2025-06-09 23:36:22 +0400

    7-hour admin panel template research and part 1 of admin panel from front to back

---
+++ MedishcMVCProject/Areas/admin/Controllers/DoctorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedishcMVCProject.Areas.admin.Controllers
+{
+    public class DoctorController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MedishcMVCProject/obj/Debug/net8.0/MedishcMVCProject.AssemblyInfo.cs
+++ MedishcMVCProject/obj/Debug/net8.0/MedishcMVCProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedishcMVCProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e275877da7a45333038c20b774f8a4544b8718a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedishcMVCProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedishcMVCProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
