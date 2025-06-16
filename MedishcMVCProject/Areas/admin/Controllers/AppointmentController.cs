ffe84d981456b38bd07e4b9fd27d687aec6e2bb5
QedirMahmudov <1qedirmahmudov@gmail.com>
2025-06-16 23:47:24 +0400

    Some research about the appointment

---
+++ MedishcMVCProject/Areas/admin/Controllers/AppointmentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedishcMVCProject.Areas.admin.Controllers
+{
+    [Area("Admin")]
+    public class AppointmentController : Controller
+    {
+        public IActionResult Appointments()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
--- MedishcMVCProject/obj/Debug/net8.0/MedishcMVCProject.AssemblyInfo.cs
+++ MedishcMVCProject/obj/Debug/net8.0/MedishcMVCProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedishcMVCProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1724185b2c8c5384acdb297f79ca8d2cbe35836")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cbb7cc219e1dbacdde61fc0402a6a33785c69d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedishcMVCProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedishcMVCProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
