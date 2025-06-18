2b4034c39a7b1db4dabddb2632b026b867e13634
QedirMahmudov <1qedirmahmudov@gmail.com>
2025-06-19 01:31:45 +0400

    appointment research end & salary front to back

---
+++ MedishcMVCProject/Areas/admin/Controllers/SalaryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedishcMVCProject.Areas.admin.Controllers
+{
+    [Area("Admin")]
+    public class SalaryController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
