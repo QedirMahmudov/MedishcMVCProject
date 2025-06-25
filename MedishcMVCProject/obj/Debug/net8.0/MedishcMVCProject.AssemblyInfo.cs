4f4be11dade43932e9ad99603bbc3ef0f0cc5a4e
QedirMahmudov <1qedirmahmudov@gmail.com>
2025-06-25 22:40:48 +0400

    adminpanelfix

--- MedishcMVCProject/obj/Debug/net8.0/MedishcMVCProject.AssemblyInfo.cs
+++ MedishcMVCProject/obj/Debug/net8.0/MedishcMVCProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedishcMVCProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce441f49e7bb85a4752d7a7f79339881633879b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e20865dd9b5cf893a05068526b8b37f53a26387")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedishcMVCProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedishcMVCProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
